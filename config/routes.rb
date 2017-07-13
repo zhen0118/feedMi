8cb796220c8247a919039e07a9ad867e7fb4930c
vincentltung <vincentltung@gmail.com>
2017-07-13 15:34:19 -0700

    Changed welcome to home

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :users
-  #get 'welcome/index'
+  #get 'home/index'
   match ':controller(/:action(/:id))' , :via => :get
-  root 'welcome#index'
+  root 'home#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
