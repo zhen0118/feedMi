73a5b27b52d8049229968d69aae930c80fc14456
Vincent Tung <vincentltung@gmail.com>
2017-12-25 23:02:45 -0800

    Added beginning of sessions

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,11 +1,15 @@
 class ProfileController < ActionController::Base
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
   # GET /users
   def index
     @users = User.all
    # @user = User.find(params[:id])||current_user
   end
-  # GET /posts/1
-	def show
-	  @user = User.find(params[:id])
-	end
+
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
 end
