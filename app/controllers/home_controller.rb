a14d7a06ee562ce0837759224acffb5cb0386f06
unknown <30558687+heubueno@users.noreply.github.com>
2018-03-24 16:15:39 -0700

    Added rudimentary food type filter

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,9 +8,14 @@ class HomeController < ApplicationController
 
   def food
   	@food_origin = params[:food_origin]
+  	@food_type = params[:food_type]
   	#TODO: add parameter checking
-  	if @food_origin != ""
-  		@food = Food.where("food_origin = ?", @food_origin).sample
+  	if @food_origin != "" && @food_type != ""
+  		@food = Food.where("food_origin = ? and food_type = ?", @food_origin, @food_type).sample
+  	elsif @food_origin != ""
+  		@food = Food.where("food_origin =?", @food_origin).sample
+  	elsif @food_type != ""
+  		@food = Food.where("food_type =?", @food_type).sample
   	else
   		@food = Food.order("RAND()").first
   	end
