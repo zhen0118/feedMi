1b92047659335538840bd9b46070e8fd2eb2da70
ozhen@sfu.ca <ozhen@sfu.ca>
2018-04-22 16:34:45 -0700

    weather function is added

--- Gemfile
+++ Gemfile
@@ -36,6 +36,8 @@ gem 'jbuilder', '~> 2.5'
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.11'
+gem 'openweather2'
+gem 'httparty'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,10 +1,24 @@
+require 'url_api'
+
 class ProfileController < ActionController::Base
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def create
+    
+    # etc
+  end
+
+
   # GET /users
   def index
     @users = User.all
+
+    @weather = HTTParty.get('http://api.openweathermap.org/data/2.5/weather?q=vancouver,ca&appid=14690f349339811822071d5e32bc915f&units=metric',
+    :headers =>{'Content-Type' => 'application/json'} )
     #@username = User.find(params[:user_name])||current_user
   end
+
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
